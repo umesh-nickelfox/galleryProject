66e417928e206de6c158c05d7dea8045275a404d
umesh <umesh@nickelfox.com>
2021-12-01 13:47:36 +0530

    'firstcommit'

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, FormGroup, FormLabel, Button } from "react-bootstrap";
+
 
 const Login = () => {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validation() {
+    return email.length > 0 && password.length > 0;
+  }
+  function handelSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handelSubmit}>
+        <FormGroup size="lg" controlId="password">
+          <FormLabel>Password</FormLabel>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <Button block size="lg" type="submit" disabled={!validation()}>Login</Button>
+      </Form>
+    </div>);
 };
 
 export default Login;
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 
 const SignUp = () => {
-  return <div></div>;
+  const [email, setEmail] = useState();
+  const [emailError, setEmailError] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [confirmPasswordError, setConfirmPasswordError] = useState();
+
+  const [password, setPassword, passwordError] = usePasswordValidator({
+    min: 8,
+    max: 15
+  });
+  useEffect(
+    () => {
+      if (!email) {
+        setEmailError("");
+      }
+      else {
+        if (validateEmail(email)) {
+          setEmailError("");
+        }
+        else {
+          setEmailError("Please enter valid email");
+        }
+      }
+    }, [email]
+  );
+  useEffect(
+    () => {
+      if (!confirmPassword || !password) {
+        setConfirmPasswordError("")
+      }
+      else {
+        if (password !== confirmPassword) {
+          setConfirmPasswordError("Password must match");
+        }
+        else {
+          setConfirmPasswordError("");
+        }
+      }
+    }, [password, confirmPassword]
+  );
+  return (<div>
+    <form>
+      <h3>Sign Up</h3>
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        type="text"
+        placeholder="Email"
+      />
+      <div className="error">{emailError}</div>
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        type="text"
+        placeholder="Password"
+      />
+      <div className="error">{passwordError}</div>
+      <input
+        value={confirmPassword}
+        onChange={e => setConfirmPassword(e.target.value)}
+        type="text"
+        placeholder="confirm Password"
+      />
+    </form>
+  </div>);
 };
 
 export default SignUp;
